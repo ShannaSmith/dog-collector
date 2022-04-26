11545a8b769d592e555b66517d536360f1607e8a
ShannaSmith <shannagjh@gmail.com>
2022-04-25 20:05:29 -0400

    add admin

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,6 +1,6 @@
 #from xml.dom import domreg
 from django.shortcuts import render
-
+from .models import Dog
 # Create your views here.
 from django.http import HttpResponse
 #Add the dog class & list and view function below the imports
@@ -11,15 +11,12 @@ class Dog:  # Note that parens are optional if not inheriting from another class
     self.description = description
     self.age = age
 
-dogs = [
-  Dog('Twilight', 'Havapoo', 'evil', 8),
-  Dog('Sage', 'Giant Schnauzer', 'black and playful', 2),
-  Dog('Nyla', 'Pomeranian', 'neurotic', 4)
-]
+
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 def about(request):
     return  render(request, 'about.html')
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
