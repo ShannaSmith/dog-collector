4334c6dc28b2574f831648b6808f78ab6662acd4
ShannaSmith <shannagjh@gmail.com>
2022-04-26 22:45:01 -0400

    fix delete syntax errors

--- dogcollector/main_app/urls.py
+++ dogcollector/main_app/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
      # new route used to show a form and create a dog
      path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
      path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
-     path('dogs/<int:pk>/delete', views.DogDelete.as_view(), name='dogs_delete'),
+     path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete'),
 ]
