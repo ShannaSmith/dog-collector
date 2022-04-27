903583748a132caeef5d42dcf6490e11f2725729
ShannaSmith <shannagjh@gmail.com>
2022-04-26 20:49:12 -0400

    correct indentation on views and urls

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -6,6 +6,18 @@ from .models import Dog # importing my model
 # Create your views here.
 from django.http import HttpResponse
 
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+class DogUpdate(UpdateView):
+    model = Dog
+    # disallow the renaming of a dog by excluding the name field
+    fields = ['breed', 'description', 'age']
+    
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
+    
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
@@ -18,15 +30,3 @@ def dogs_detail(request, dog_id): # path('dogs/<int:dog_id>/') - this is where d
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', {'dog': dog})
 
-class DogCreate(CreateView):
-    model = Dog
-    fields = '__all__'
-class DogUpdsate(UpdateView):
-    model = Dog
-    # disallow the renaming of a dog by excluding the name field
-    fields = ['breed', 'description', 'age']
-    
-class DogDelete(DeleteView):
-    model = Dog
-    success_url = '/cats/'
-    
