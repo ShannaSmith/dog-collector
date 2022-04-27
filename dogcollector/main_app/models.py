02f714f07399bdcf5374f923a8065d96d8c8ab0a
ShannaSmith <shannagjh@gmail.com>
2022-04-26 23:23:51 -0400

    add one to many model

--- dogcollector/main_app/models.py
+++ dogcollector/main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
-
+# A tuple of 2-tuples
+Outtings =(
+    ('W', 'walk'),
+    ('H', 'Hike'),
+    ('P', 'Dog Park')
+)
 # Create your models here.
 class Dog(models.Model):
     name = models.CharField(max_length=100)
@@ -11,3 +16,13 @@ class Dog(models.Model):
         return f"The dog named {self.name} has id of {self.id}"
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
+class Exercise(models.Model):
+    date = models.DateField()
+    outting = models.CharField(
+        max_length=1,
+        #add the 'choices' field option
+        choices=Outtings,
+       # set the default value for outting to be 'W'
+       default=Outtings[0][0]
+        )
+    
